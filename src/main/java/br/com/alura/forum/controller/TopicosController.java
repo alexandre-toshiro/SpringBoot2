717be03a3c11e1d972af20ec98cba6b85c59c4c7
Toshiro <toshiro.lyn@gmail.com>
2020-10-01 20:35:21 -0300

    Ordenando a paginacao

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,9 +44,10 @@ public class TopicosController {
 	
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, @RequestParam int qtd) {
+			@RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd); //faz a paginação
+		//Acrescentando ordem a paginacao
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao); //faz a paginação
 		
 		//trocar o todos os tipo de LIst para Page.
 		if (nomeCurso == null) {
