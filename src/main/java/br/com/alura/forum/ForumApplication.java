72036644bdbe4acf67b57e85aaf8f6e573151f87
Toshiro <toshiro.lyn@gmail.com>
2020-10-01 22:06:29 -0300

    Utilizando cache

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,10 +2,12 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport // Spring pega dados da requisição(param da url) as info de paginacao e repasse ao Spring data.
+@EnableCaching
 public class ForumApplication {
 
 	public static void main(String[] args) {
