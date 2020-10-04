7674c58bb9542e83c022c438f717d4e75a609177
Toshiro <toshiro.lyn@gmail.com>
2020-10-04 14:18:38 -0300

    Complementando a validacao

--- src/main/java/br/com/alura/forum/controller/form/LoginForm.java
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -1,8 +1,12 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+
 public class LoginForm {
 
+	@NotEmpty
 	private String email;
+	@NotEmpty
 	private String senha;
 
 	public String getEmail() {
